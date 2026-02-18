7928f4a4604e59e654f8610db1a1024eeddd66ef
shynivj <58973792+shynivj@users.noreply.github.com>
2026-02-18 17:08:39 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -2,6 +2,6 @@ public class Student
 {   
       public static void main(String[] args)
       {       
-         System.out.println("Hello gist @ GitHub!");   
+         System.out.println("Hello git @ GitHub!");   
       }
 } 
